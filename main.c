1c3a7c52d6d399dc5a6c50cb7aa790c032dd5a42
bgamble01 <73133168+bgamble01@users.noreply.github.com>
2022-02-11 23:18:15 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /*
  ============================================================================
  Name        : PolishSol.c
- Author      : 
+ Author      : bgamble01
  Version     :
  Copyright   : Your copyright notice
  Description : Hello World in C, Ansi-style
@@ -19,10 +19,31 @@
 int main()
 {
 	char eqn[MAX_EQ_LEN];	 // string to store the input RPN expression
-	char resp[MAX_EQ_LEN]; // string to store the user's response "y" or "n"
+	char resp[MAX_EQ_LEN];  //string to store the user's response "y" or "n"
 
-	/* TODO */
+	int program=1;
+	double ans;
 
+	while(program == 1){
+		printf("\nEnter an equation (in RPN): \n");
+		fgets(eqn, MAX_EQ_LEN-1,stdin);
+		ans = evaluate(eqn);
+		if(isnan(ans)){
+		
+		}
+		else{
+			printf("\n%.3f\n\n",ans);
+		}
+
+		printf("Evaluate another? (y/n): ");
+		fgets(resp, MAX_EQ_LEN-1,stdin);
+		if(resp[0]=='y'){
+
+		}
+		else{
+			program = 0;
+		}
+	}
 	printf("Exiting...\n");
 	return 0;
 }
